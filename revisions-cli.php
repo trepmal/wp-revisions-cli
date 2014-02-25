123c6be3b696322c70b7b75c800385c9568bc05c
Kailey Lampert <trepmal@gmail.com>
2014-02-24 20:11:05 -0800

    remove temp function

--- revisions-cli.php
+++ revisions-cli.php
@@ -1,9 +1,4 @@
 <?php
-function convert($size)
- {
-    $unit=array('b','kb','mb','gb','tb','pb');
-    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
- }
 /**
  * Manage Revisions
  */

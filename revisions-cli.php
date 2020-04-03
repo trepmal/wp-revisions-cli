07eb5c1b3a7b9b596f1541955d53fa33a8bcf1d1
Kailey Lampert <trepmal@gmail.com>
2020-04-03 09:10:53 -0700

    A bit of housecleaning

--- revisions-cli.php
+++ revisions-cli.php
@@ -1,17 +1,21 @@
 <?php
 /**
- * Revisions CLI
+ * Plugin Name: Revisions CLI
+ * Plugin URI: https://github.com/trepmal/wp-revisions-cli/
+ * Description: Plugin wrapper for WP CLI command, WP Revisions CLI
+ * Version: 0.1.0
+ * Author: Kailey Lampert
+ * Author URI: kaileylampert.com
+ * License: MIT
+ * TextDomain:
+ * DomainPath:
+ * Network:
  *
  * @package trepmal/wp-revisions-cli
  */
 
+if ( ! defined( 'WP_CLI' ) ) return;
 
-if ( ! defined( 'WP_CLI' ) ) {
-	return;
-}
-
-if ( ! class_exists( 'Revisions_CLI' ) ) {
-	require __DIR__ . '/inc/class-revisions-cli.php';
-}
+require_once __DIR__ . '/inc/class-revisions-cli.php';
 
 WP_CLI::add_command( 'revisions', 'Revisions_CLI' );
